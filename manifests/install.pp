339dd0b5ddb1d994c4eef7a96542e09eae339e33
Derrick Ampy <dsampy@gmail.com>
2014-02-17 17:55:17 -0500

    Updated install Puppet classes to download artifacts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,16 @@
 class httpd::install {
+	wget::fetch { "Download HTTPD":
+	  source      => 'http://mirror.reverse.net/pub/apache//httpd/httpd-2.4.7.tar.gz',
+	  destination => '/tmp/httpd-2.4.7.tar.gz',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 
+	wget::fetch { "Download APR":
+	  source      => 'http://www.motorlogy.com/apache//apr/apr-1.5.0.tar.gz',
+	  destination => '/tmp/apr-1.5.0.tar.gz',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 }
+
