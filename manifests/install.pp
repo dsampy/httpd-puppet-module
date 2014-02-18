695d0a958ec00b50ec4d0de5da9a5b49ad5c5c20
Derrick Ampy <dsampy@gmail.com>
2014-02-17 20:26:27 -0500

    Updated Puppet install classes to use nexus repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,13 @@
 class httpd::install {
 	wget::fetch { "Download HTTPD":
-	  source      => 'http://mirror.reverse.net/pub/apache//httpd/httpd-2.4.7.tar.gz',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/apache/httpd/httpd/2.4.7/httpd-2.4.7.tar.gz',
 	  destination => '/tmp/httpd-2.4.7.tar.gz',
 	  timeout     => 0,
 	  verbose     => false,
 	}
 
 	wget::fetch { "Download APR":
-	  source      => 'http://www.motorlogy.com/apache//apr/apr-1.5.0.tar.gz',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/apache/apr/apr/1.5.0/apr-1.5.0.tar.gz',
 	  destination => '/tmp/apr-1.5.0.tar.gz',
 	  timeout     => 0,
 	  verbose     => false,
